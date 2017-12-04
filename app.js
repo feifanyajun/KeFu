d178a14905eb06892fd6216a215b728d80364150
feifanyajun <870454840@qq.com>
2017-12-04 15:01:23 +0800

    备注
    备注

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');//图标
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
